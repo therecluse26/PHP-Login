776f7c258a49b1e521f3f2532dae984e7b1cb30e
Fethi El Hassasna <e.fethi.c@gmail.com>
2013-11-05 23:17:02 +0100

    Add Bootstrap js file

--- main_login.php
+++ main_login.php
@@ -35,6 +35,8 @@
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
     <!-- The AJAX login script -->
     <script src="js/login.js"></script>
     
