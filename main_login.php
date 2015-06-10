7a0b29503b8c03129690ab880c5b329fc19fd3fe
Yums <yums467@nenu.edu.cn>
2015-06-10 21:14:58 +0800

    enable the function of "Remember Me"
    1.add a link of javascript .
    2.enable the HTML element checkbox  "remember-me",set the id of checkbox as "checkme".
    3.call the function in js (checkCookie() a part of the function of RememberMe)  in HTML <Body> onLoad.

--- main_login.php
+++ main_login.php
@@ -2,7 +2,7 @@
   session_start();
 
   if(isset($_SESSION['username'])){
-    header("location:index.php");
+      header("location:index.php");
   }
 ?><!DOCTYPE html>
 <html lang="en">
@@ -13,28 +13,29 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="css/main.css" rel="stylesheet" media="screen">
+    <script type="text/javascript" src="js/rememberMe.js"></script>
   </head>
   
-  <body>
+  <body onLoad="checkCookie()">
     <div class="container">
 
       <form class="form-signin" name="form1" method="post" action="checklogin.php">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input name="myusername" id="myusername" type="text" class="form-control" placeholder="Username" autofocus>
         <input name="mypassword" id="mypassword" type="password" class="form-control" placeholder="Password">
-        <!-- The checkbox remember me is not implemented yet...
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        -->
-        <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+     <!-- The checkbox remember me implemented -->
+      <label class="checkbox">
+          <input id="checkme" type="checkbox" value="remember-me"> Remember me
+      </label>
+        
+      <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 
-        <div id="message"></div>
+      <div id="message"></div>
       </form>
 
-    </div> <!-- /container -->
+      </div> <!-- /container -->
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script type="text/javascript" src="js/bootstrap.js"></script>
