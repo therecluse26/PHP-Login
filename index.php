d7a6530826a96dfcf25ea5a72f69fd5dab3fe305
Sainag Shetty <shettysainag@gmail.com>
2015-09-20 16:12:43 +0530

    Minor change

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
   if(!isset($_SESSION['username'])){
     header("location:main_login.php");
   }
+  else{
+  echo "Welcome  ".$_SESSION['username'];
+}
 ?><!DOCTYPE html>
 <html lang="en">
   <head>
