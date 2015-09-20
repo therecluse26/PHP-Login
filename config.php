99cd9bb2a341d0699ae476f3a908fdbd1278ff47
Sainag Shetty <shettysainag@gmail.com>
2015-09-20 16:11:23 +0530

    Using password hashing

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 	$host="localhost"; // Host name 
 	$username="root"; // Mysql username 
-	$password="root"; // Mysql password 
+	$password=""; // Mysql password 
 	$db_name="login"; // Database name 
 	$tbl_name="members"; // Table name
+	$salt = "batman is here" // For SHA1 hashing
 ?>
