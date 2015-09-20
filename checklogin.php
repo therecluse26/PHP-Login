ffad1e7be26e7511e6037ea23c765804fbe9a033
Sainag Shetty <shettysainag@gmail.com>
2015-09-20 16:09:35 +0530

    Switching back to mysql functions

--- checklogin.php
+++ checklogin.php
@@ -5,35 +5,33 @@
 	include_once 'config.php';
 
 	// Connect to server and select databse.
-	try
-	{
-		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-		$db = new PDO('mysql:host='.$host.';dbname='.$db_name.';charset=utf8', $username, $password);
-	}
-	catch(Exception $e)
-	{
-		die('Error : ' . $e->getMessage());
-	}	
+	mysql_connect("$host", "$username", "$password")or die("cannot connect");
+	mysql_select_db("$db_name")or die("cannot select DB");
 
+
+	$value = isset($_POST['myemail']) ? $_POST['myemail'] : '';
 	// Define $myusername and $mypassword 
-	$myusername = $_POST['myusername']; 
+	$myemail = $value;
 	$mypassword = $_POST['mypassword']; 
-
 	// To protect MySQL injection
-	$myusername = stripslashes($myusername);
+	$myusername = stripslashes($myemail);
 	$mypassword = stripslashes($mypassword);
-		
-	$stmt = $db->query("SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'");
+	$myusername = mysql_real_escape_string($myusername);
+	$mypassword = mysql_real_escape_string($mypassword);
+	$mypassword = sha1($mypassword.$salt);
+
+	$sql="SELECT * FROM $tbl_name WHERE email='$myusername' and password='$mypassword'";
+	$result=mysql_query($sql);
 
 	// rowCount() is counting table row
-	$count = $stmt->rowCount();
-
+	$count=mysql_num_rows($result);
+	$row = mysql_fetch_array($result,MYSQL_ASSOC);
 	// If result matched $myusername and $mypassword, table row must be 1 row
 	if($count == 1){
 
 		// Register $myusername, $mypassword and print "true"
 		echo "true";
-		$_SESSION['username'] = 'myusername';
+		$_SESSION['username'] = $row['username'];
 		$_SESSION['password'] = 'mypassword';
 		
 	}
