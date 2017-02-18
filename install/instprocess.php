dcf3f725940e6af7d3d452251b3898285e5e1518
= <braddmagyar@gmail.com>
2017-02-18 10:28:44 -0500

    ...

--- install/instprocess.php
+++ install/instprocess.php
@@ -1,80 +1,81 @@
 <?php
-if(!isset($_SESSION)) {
-    session_start();
-}
-require "installscript.php";
+try {
 
-set_time_limit(5000);
-
-$resp = '';
-$failure = 0;
-$total = 23;
-$i = 0;
-$status = '';
-
-$dbhost = $_POST['dbhost'];
-$dbuser = $_POST['dbuser'];
-$dbpw = $_POST['dbpw'];
-$dbname = $_POST['dbname'];
-$tblprefix = $_POST['tblprefix'];
-$superadmin = $_POST['superadmin'];
-$saemail = $_POST['saemail'];
-$said = $_POST['said'];
-$sapw = $_POST['sapw'];
-$base_dir = base64_decode($_POST['base_dir']);
-$base_url = base64_decode($_POST['base_url']);
-$site_name = $_POST['site_name'];
-
-$arr_content = array();
-$settingsArr = array("base_dir"=>$base_dir, "base_url"=>$base_url, "site_name"=>$site_name);
-
-while ($i < $total) {
-
-    if ($failure == 1) {
-        break;
+    if(!isset($_SESSION)) {
+        session_start();
     }
-    else {
-        if($i <= 18 && $i <> 16) {
+    require "installscript.php";
 
-            $statobj = installDb($i, $dbhost, $dbname, $dbuser, $dbpw, $tblprefix, $superadmin, $saemail, $said, $sapw);
+    set_time_limit(5000);
 
-        } elseif ($i == 16 || $i == 19) {
+    $resp = '';
+    $failure = 0;
+    $total = 23;
+    $i = 0;
+    $status = '';
 
-            //Insert basic app settings
-            $statobj = installDb($i, $dbhost, $dbname, $dbuser, $dbpw, $tblprefix, $superadmin, $saemail, $said, $sapw, $settingsArr);
+    $dbhost = $_POST['dbhost'];
+    $dbuser = $_POST['dbuser'];
+    $dbpw = $_POST['dbpw'];
+    $dbname = $_POST['dbname'];
+    $tblprefix = $_POST['tblprefix'];
+    $superadmin = $_POST['superadmin'];
+    $saemail = $_POST['saemail'];
+    $said = $_POST['said'];
+    $sapw = $_POST['sapw'];
+    $base_dir = base64_decode($_POST['base_dir']);
+    $base_url = base64_decode($_POST['base_url']);
+    $site_name = $_POST['site_name'];
 
-        } elseif ($i == 20) {
+    $arr_content = array();
+    $settingsArr = array("base_dir"=>$base_dir, "base_url"=>$base_url, "site_name"=>$site_name);
 
-            require "instcomposer.php";
+    while ($i < $total) {
 
-        } else {
-
-            $refurl = urlencode($base_url."/admin/editconfig.php");
-
-            $statobj['status'] = 'Installation Complete!<br>
-            <a href="'.$base_url.'/login/index.php?refurl='.$refurl.'">Sign In And Finish Configuration</a>';
-
-            $statobj['failure'] = 0;
+        if ($failure == 1) {
+            break;
         }
+        else {
+            if($i <= 18 && $i <> 16) {
 
-        $percent = intval(($i+1)/$total * 100);
+                $statobj = installDb($i, $dbhost, $dbname, $dbuser, $dbpw, $tblprefix, $superadmin, $saemail, $said, $sapw);
 
-        $arr_content['percent'] = $percent;
-        $arr_content['message'] = $statobj['status'];
-        $arr_content['failure'] = $statobj['failure'];
+            } elseif ($i == 16 || $i == 19) {
 
-        if (is_writable("tmp")){
+                //Insert basic app settings
+                $statobj = installDb($i, $dbhost, $dbname, $dbuser, $dbpw, $tblprefix, $superadmin, $saemail, $said, $sapw, $settingsArr);
+
+            } elseif ($i == 20) {
+
+                require "instcomposer.php";
+
+            } else {
+
+                $refurl = urlencode($base_url."/admin/editconfig.php");
+
+                $statobj['status'] = 'Installation Complete!<br>
+                <a href="'.$base_url.'/login/index.php?refurl='.$refurl.'">Sign In And Finish Configuration</a>';
+
+                $statobj['failure'] = 0;
+            }
+
+            $percent = intval(($i+1)/$total * 100);
+
+            $arr_content['percent'] = $percent;
+            $arr_content['message'] = $statobj['status'];
+            $arr_content['failure'] = $statobj['failure'];
 
             file_put_contents("tmp/" . session_id() . ".txt", json_encode($arr_content));
 
-        } else {
-            echo "Can't write";
+            $sleep = rand(50000,500000);
+            usleep($sleep);
+
+            unset($conn);
+            $i++;
         }
-
-        $sleep = rand(50000,500000);
-        usleep($sleep);
-
-        unset($conn);
-        $i++;
     }
-};
+
+} catch (Exception $e) {
+
+    echo $e->getMessage();
+}
--- login/partials/libincludes.php
+++ login/partials/libincludes.php
@@ -1,4 +1,6 @@
 <?php
+echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script><link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
+
 switch ($title) {
     case "Reset Password":
         echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
@@ -9,62 +11,47 @@ switch ($title) {
         break;
 
     case "Sign Up":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/login/js/signup.js"></script>';
         echo '<script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>';
         echo '<script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         break;
 
     case "Forgot Password":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/login/js/forgotpassword.js"></script>';
         echo '<script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>';
         echo '<script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         break;
 
     case "Admin Verification":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/adminverifyuser.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/admindeleteuser.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/adminselectall.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/admincheckrow.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         break;
 
     case "Mail Log":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/admindeletelog.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/admincheckrow.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/forwardback.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         break;
 
     case "Edit Site Configuration":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/admin/js/editconfig.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         break;
 
     case "Edit Profile":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/user/js/profileupdate.js"></script>';
         echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.4.0/croppie.min.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen"><link href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.4.0/croppie.min.css" rel="stylesheet" media="screen">';
+        echo '<link href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.4.0/croppie.min.css" rel="stylesheet" media="screen">';
         break;
 
     case "Account Settings":
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
         echo '<script src="'.$this->base_url.'/user/js/accountupdate.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         echo '<script src="http://jqueryvalidation.org/files/dist/jquery.validate.min.js"></script>';
         echo '<script src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>';
         break;
 
     default:
-        echo '<script src="'.$this->base_url.'/login/vendor/components/jquery/jquery.min.js"></script><script src="'.$this->base_url.'/login/vendor/components/bootstrap/js/bootstrap.min.js"></script>';
-        echo '<link href="'.$this->base_url.'/login/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"><link href="'.$this->base_url.'/login/css/main.css" rel="stylesheet" media="screen">';
         break;
 
 }
