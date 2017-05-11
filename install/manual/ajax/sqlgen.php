96a0ba2c8c5bdaa36a73fd7faf71f1e713ff5e3e
Magyar <Brad.Magyar@mrisoftware.com>
2017-05-11 15:46:16 -0400

    Small tweaks...

--- install/manual/ajax/sqlgen.php
+++ install/manual/ajax/sqlgen.php
@@ -14,29 +14,34 @@ $sa_email = $_POST['sa_email'];
 
 $pw_plain = $_POST['sa_password'];
 
-$file = $_SERVER['DOCUMENT_ROOT']."/"."sql/PHPLoginDB.txt";
+try {
 
+    $file = __DIR__."/"."sql/phplogindb.txt";
 
-$sql = file_get_contents($file);
+    $sql = file_get_contents($file);
 
-$_POST['sa_password'] = password_hash($pw_plain, PASSWORD_DEFAULT);
+    $_POST['sa_password'] = password_hash($pw_plain, PASSWORD_DEFAULT);
 
-if (preg_match_all("/{{(.*?)}}/", $sql, $m)) {
-  foreach ($m[1] as $i => $varname) {
-    $sql = str_replace($m[0][$i], sprintf('%s', '\''.$_POST[$varname].'\''), $sql);
-  }
+    if (preg_match_all("/{{(.*?)}}/", $sql, $m)) {
+      foreach ($m[1] as $i => $varname) {
+        $sql = str_replace($m[0][$i], sprintf('%s', '\''.$_POST[$varname].'\''), $sql);
+      }
+    }
+
+    echo '<b>1) Copy/Paste this SQL statement into your SQL client and execute as user with root privileges</b><br>
+        <textarea id="sqlResults" class="form-control" rows="10" cols="70">'. $sql .'</textarea>
+        <button id="copySql" class="btn btn-default col-md-4">Copy to Clipboard</button><span class="col-md-4" id="copyNotifSql"></span><br>
+        <br><script>
+         $("#copySql").click(function(){
+
+            $("#sqlResults").select();
+            document.execCommand("copy");
+
+            $("#copyNotifSql").html("Copied text to clipboard</div>").fadeOut(2000);
+
+        });</script>';
+    
+} catch (Exception $e) {
+    
+    echo 'Error: ' . $e->getMessage();
 }
-
-echo '<b>1) Copy/Paste this SQL statement into your SQL client and execute as user with root privileges</b><br>
-    <textarea id="sqlResults" class="form-control" rows="10" cols="70">'. $sql .'</textarea>
-    <button id="copySql" class="btn btn-default col-md-4">Copy to Clipboard</button><span class="col-md-4" id="copyNotifSql"></span><br>
-    <br><script>
-     $("#copySql").click(function(){
-
-        $("#sqlResults").select();
-        document.execCommand("copy");
-
-        $("#copyNotifSql").html("Copied text to clipboard</div>").fadeOut(2000);
-
-    });</script>';
-
