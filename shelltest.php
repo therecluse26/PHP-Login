f55a2c086892a5b63babca8dd20b033c58eff40c
Brad Magyar <braddmagyar@gmail.com>
2017-12-02 23:28:38 -0500

    Cleaning up

--- shelltest.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ostype = php_uname("s");
-$osrelease = php_uname("r");
-
-echo "OS: ". $ostype;
-echo "<br>Release: ". $osrelease . "<br>";
-
-echo ( shell_exec(".\\install\\bin\\php.exe 'echo \"blah\"'; 2>&1") );
