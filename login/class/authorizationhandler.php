087b08dd0d9099ed3a50d13987d90a30e2945bb7
Daniel Falk <daniel@da-robotteknik.se>
2018-02-13 19:02:36 +0100

    Refactor authorization
    * More reusable and easier structure
    * Fixes bug:
        Missing exit after location header might leak superadminpages to admins
    * Fixes bug:
        refurl redirect loop preventing users from logging in if refurl contains
        the address of a adminpage

---
+++ login/class/authorizationhandler.php
@@ -0,0 +1,60 @@
+<?php
+
+// Verifies a users authorization
+class AuthorizationHandler
+{
+
+    //Checks session keys
+    private function checkSessionKey($key)
+    {
+        if (!isset($_SESSION[$key])) {
+            return false;
+        }
+        return $_SESSION[$key];
+    }
+
+    //Check if pagetype is a known value
+    private function checkPageType($pagetype)
+    {
+        $allowed_types = array('superadminpage', 'adminpage', 'userpage', 'loginpage', 'page');
+        if (!in_array($pagetype, $allowed_types, true)) {
+            throw new Exception("Server Error: Please contact site administrator and relay this error - xER41400 [".$pagetype."]");
+            exit;
+        }
+    }
+
+    public function isSuperAdmin()
+    {
+        return $this->checkSessionKey("superadmin") == 1;
+    }
+
+    public function isAdmin()
+    {
+        return $this->checkSessionKey("admin") != false;
+    }
+
+    public function isLoggedIn()
+    {
+        return $this->checkSessionKey("username") != false;
+    }
+
+    //Check if user is OK for $pagetype
+    public function pageOk($pagetype)
+    {
+        $this->checkPageType($pagetype);
+
+        if (!$this->isSuperAdmin() && $pagetype == "superadminpage") {
+            return false;
+        }
+
+        if (!$this->isAdmin() && $pagetype == "adminpage") {
+            return false;
+        }
+        
+        if (!$this->isLoggedIn() && $pagetype == "userpage") {
+            return false;
+        }
+
+        return true;
+    }
+}
